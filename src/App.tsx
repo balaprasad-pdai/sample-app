7a6b31d193950ee349f7e30a56c15b6747f6d3d2
balaprasad-pdai <bala@populardigital.ai>
2025-11-11 10:24:32 +0530

    changed names

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>{appName}</h1>
+      <h1>{appName} demo</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
